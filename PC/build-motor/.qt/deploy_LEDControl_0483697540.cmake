8b0609d3bb1721a177034aad101741c6f7801289
Noky Alrizqi <basememes7@gmail.com>
2026-09-03 14:48:40 +0700

    remove motrol control folder qt

--- PC/build-motor/.qt/deploy_LEDControl_0483697540.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/pb123/Documents/Kuliah/pa/stm32/SDV_Corner/PC/build-motor/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/LEDControl-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/pb123/Documents/Kuliah/pa/stm32/SDV_Corner/PC/build-motor/LEDControl.exe"
-    GENERATE_QT_CONF
-)
